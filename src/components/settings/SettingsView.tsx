07298d819c4556a411ba106bdd4dd7fb14556cf9
Aiva.io <aivaioapp@gmail.com>
2025-12-20 13:46:17 +1000

    Fix JSX syntax error: add missing false branch to ternary operator

--- src/components/settings/SettingsView.tsx
+++ src/components/settings/SettingsView.tsx
@@ -1154,7 +1154,7 @@ export function SettingsView({ workspaceId, userId, user, billingContent }: Sett
                               </Button>
                             </div>
                           </div>
-                        )}
+                        ) : null}
 
                         <Separator />
 
